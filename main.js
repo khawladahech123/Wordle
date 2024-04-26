ccbbfc81e161cd41b5a726506e39b4a0a7be33d9
unknown <khawladahech6@gmail.com>
2024-04-26 14:55:54 +0200

    part 1

--- main.js
+++ main.js
@@ -22,4 +22,62 @@ function initialize() {
       document.getElementById("board").appendChild(tile);
     }
   }
+
+
+  //listen for key Press
+  document.addEventListener("keyup", (e) => {
+    if (gameOver) return; 
+    //alert(e.code);
+    if ("KeyA" <= e.code && e.code <= "KeyZ") {
+      if (col < width) {
+          let currTile = document.getElementById(row.toString() + '-' + col.toString());
+          if (currTile.innerText == "") {
+              currTile.innerText = e.code[3];
+              col+=1;
+        }
+      }
+    }
+    else if (e.code == "Backspace") {
+      if (0 < col && col <= width) {
+          col -=1;
+      }
+      let currTile = document.getElementById(row.toString() + '-' + col.toString());
+      currTile.innerText = "";
+  }
+    else if (e.code=="Enter"){
+      update();
+      row+=1;
+      col+=0;
+    }
+    if(!gameOver && row == height){
+      gameOver = true;
+      document.getElementById("answer").innerText = word ;
+    }
+
+  })
+
+}
+function update() {
+  let correct = 0;
+  for (let c = 0; c < width; c++) {
+      let currTile = document.getElementById(row.toString() + '-' + c.toString());
+      let letter = currTile.innerText;
+
+      //Is it in the correct position?
+      if (word[c] == letter) {
+          currTile.classList.add("correct");
+          correct += 1;
+      } // Is it in the word?
+      else if (word.includes(letter)) {
+          currTile.classList.add("present");
+      } // Not in the word
+      else {
+          currTile.classList.add("absent");
+      }
+
+      if (correct == width) {
+          gameOver = true;
+      }
+
+  }
 }
